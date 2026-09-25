d1ef9f1d2726f3bcd592f8ce86db0b4ce036180c
Santiago Acosta Villa Abrille <acosta.s@octubre.org.ar>
2026-09-25 19:41:27 -0300

    Support Docker installation on Ubuntu 20.04

--- install.sh
+++ install.sh
@@ -8,7 +8,15 @@ fi
 
 if ! command -v docker >/dev/null 2>&1; then
   apt-get update
-  DEBIAN_FRONTEND=noninteractive apt-get install -y docker.io docker-compose-v2
+  DEBIAN_FRONTEND=noninteractive apt-get install -y ca-certificates curl
+  install -m 0755 -d /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  chmod a+r /etc/apt/keyrings/docker.asc
+  . /etc/os-release
+  dpkg_arch="$(dpkg --print-architecture)"
+  echo "deb [arch=${dpkg_arch} signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu ${VERSION_CODENAME} stable" > /etc/apt/sources.list.d/docker.list
+  apt-get update
+  DEBIAN_FRONTEND=noninteractive apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 fi
 
 systemctl enable --now docker
@@ -21,4 +29,3 @@ fi
 
 docker compose up -d --build
 docker compose ps
-
